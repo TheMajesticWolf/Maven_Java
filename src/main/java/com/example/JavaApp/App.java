f4d42220281cd8e5d48d7c18276263ac03e030a0
Aditya <Aditya@email>
2024-11-21 09:15:46 -0800

    First commit

---
+++ src/main/java/com/example/JavaApp/App.java
@@ -0,0 +1,10 @@
+package com.example.JavaApp;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World! This is SE Lab Maven Java App");
+    }
+}
